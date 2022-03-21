f1422068f5513ff099e978ef3894c505ec27e49b
Daniel Molnar <daniel4molnar@gmail.com>
2022-03-21 22:17:41 +0100

    Main page design
    - Navigation bar
    - Main section including page content with anchor tags to each challenge solution
    - Footer section
    - Remove unnecessary code

--- challenges/tailwind.config.js
+++ challenges/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./challenges/index.html'],
   theme: {
@@ -5,6 +7,27 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+      },
+      fontFamily: {
+        // font-family: "Poppins", "Quicksand", "Roboto", "Nunito", Helvetica, Arial, Helvetica, sans-serif;
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+        quicksand: ['Quicksand', ...defaultTheme.fontFamily.sans],
+        roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+        nunito: ['nunito', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        1: '1em',
+        1.1: '1.1em',
+        1.2: '1.2em',
+        1.4: '1.4em',
+        1.6: '1.6em',
+        2: '2em',
+      },
+      height: {
+        104: '26rem',
+        112: '28rem',
+        128: '32rem',
+        136: '34rem',
       },
     },
   },
